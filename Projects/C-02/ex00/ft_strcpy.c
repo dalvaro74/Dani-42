7922eb4deb508a342f1568ce7975eb4e0f653f10
dalvaro74 <dalvaro74@gmail.com>
2021-03-07 23:47:02 +0100

    avanzando en c-02

---
+++ Projects/C-02/ex00/ft_strcpy.c
@@ -0,0 +1,4 @@
+char	*ft_strcpy(char *dest, char *src)
+{
+	
+}
---
+++ Projects/C-02/ex00/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+
+int main ()
+{
+	char *dest ;
+	
+	str = "Hola Pepito, ay Dos mio que se me quema el cocido...";
+	
+	ft_strcpy(str);
+	return(0);
+}
+
