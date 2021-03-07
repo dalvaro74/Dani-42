e6ae631b8adf9fbb35c8c98167157ee870fd8b60
dalvaro74 <dalvaro74@gmail.com>
2021-03-07 22:37:38 +0100

    completado ex08 con ordenacion por inserccion (en teoria mas eficiente que el burbuja)

--- Projects/C-01/ex08/ft_sort_int_tab.c
+++ Projects/C-01/ex08/ft_sort_int_tab.c
@@ -12,5 +12,25 @@
 
 void	ft_sort_int_tab(int *tab, int size)
 {
+	int		i;
+	int		j;
+	int		tmp;
 
+	i = 0;
+	j = 0;
+	while (i<size)
+	{
+		while (j>0)
+		{
+			if(tab[j] < tab[j-1])
+			{
+				tmp = tab[j];
+				tab[j] = tab[j - 1];
+				tab[j - 1] = tmp;
+			}
+			j--;
+		} 
+		i++;
+		j=i;
+	}
 }
--- Projects/C-01/ex08/main.c
+++ Projects/C-01/ex08/main.c
@@ -3,24 +3,20 @@
 
 void	ft_sort_int_tab(int *tab, int size);
 
-int main ()
+int main (void)
 {
-	int tab[10] = {1,2,3,4,5,6,7,8,9,0};
-	int size;
-	int i;
-
-	//tab[0] =5;
+	int 	tab[14] = {7,3,87,11987,1,77,2,92,15,9,73,5,75,36};
+	int 	size;
+	int 	i;
 
 	size = sizeof tab / sizeof tab[0];	
 	ft_sort_int_tab(tab, size);
-
 	i=0;
-	while (i<size)
+	while (i < size)
 	{
-		printf("%d,",tab[i]);
-		i++;
+		printf("%d - ",tab[i]);
+		i++; 
 	}
-
-	return(0);
+	return (0);
 }
 
