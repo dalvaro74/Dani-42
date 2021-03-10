7525cdf803f4ce50a3b229fda93cc0dd7da027b1
dalvaro74 <dalvaro74@gmail.com>
2021-03-10 21:42:04 +0100

    super descubrimiento

--- Projects/C-02/ex00/main.c
+++ Projects/C-02/ex00/main.c
@@ -5,14 +5,19 @@ char	*ft_strcpy(char *dest, char *src);
 
 int main (void)
 {	
+	char *point;
+
 	char dest[] = "Hola Pepito, ay Dos mio que se me quema el cocido...";
 	char src[] = "Creo que esto cab";	
-	ft_strcpy(dest, src);
+	point = ft_strcpy(dest, src);
 	printf("%s\n", dest);
+	printf("%s", point);
 
+	/*
 	char dest2[] = "Hola Pepito, ay Dos mio que se me quema el cocido...";
 	char src2[] = "Creo que esto cab";
 	strcpy(dest2, src2);
 	printf("%s\n", dest2);
+	*/
 	return(0);
 }
