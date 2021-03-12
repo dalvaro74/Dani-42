a5798ea1d6f4dd814ff50b1c43293d1bec564a6e
dalvaro74 <dalvaro74@gmail.com>
2021-03-12 15:05:18 +0100

    test free malloc

---
+++ Projects/0-TEST/free-malloc/pr01.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int		main (void)
+{
+	unsigned int num_elementos;
+	int *ptr = NULL;
+
+	num_elementos = 4;
+
+	ptr = malloc(num_elementos * sizeof(int));
+	if (ptr != NULL)
+	{
+		*ptr = 45678955;
+	}
+
+	printf("%d\n", *ptr);
+
+	free(ptr);
+	return (0);
+}
