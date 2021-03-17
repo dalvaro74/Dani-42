5e73079c572a444c45e1f6461edc1e141dafaa9a
dalvaro74 <dalvaro74@gmail.com>
2021-03-17 14:59:54 +0100

    borrados zips y terminado C-06

--- Projects/C-06/ex03/ft_sort_params.c
+++ Projects/C-06/ex03/ft_sort_params.c
@@ -6,7 +6,70 @@
 /*   By: dalvaro- <dalvaro-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 20:11:38 by dalvaro-          #+#    #+#             */
-/*   Updated: 2021/03/16 20:11:39 by dalvaro-         ###   ########.fr       */
+/*   Updated: 2021/03/17 14:47:44 by dalvaro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int		print_params(int argc, char *argv[])
+{
+	int i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
+
+int		main(int argc, char *argv[])
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 1;
+	j = 1;
+	while (i < argc)
+	{
+		while (j > 0)
+		{
+			if (ft_strcmp(argv[j], argv[j - 1]) < 0)
+			{
+				tmp = argv[j];
+				argv[j] = argv[j - 1];
+				argv[j - 1] = tmp;
+			}
+			j--;
+		}
+		i++;
+		j = i;
+	}
+	print_params(argc, argv);
+	return (0);
+}
